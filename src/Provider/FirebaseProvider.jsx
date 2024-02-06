e36d86e9bfba0a3f33268ebcc354ba0b4bfe2053
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-06 22:12:22 +0600

    Project structure done

---
+++ src/Provider/FirebaseProvider.jsx
@@ -0,0 +1,9 @@
+const FirebaseProvider = () => {
+  return (
+    <div>
+      <h1>provider</h1>
+    </div>
+  );
+};
+
+export default FirebaseProvider;
---
+++ src/Routes/PrivetRoutes.jsx
@@ -0,0 +1,9 @@
+const PrivetRoutes = () => {
+  return (
+    <div>
+      <h1>Privet route</h1>
+    </div>
+  );
+};
+
+export default PrivetRoutes;
