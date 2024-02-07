c43a44580b805df7da79d2e1ac4cd773b571b448
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-07 13:28:49 +0600

    testimonal added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Banner from "./Banner";
-
+import Testimonial from "./Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <h1>Homoe</h1>
+      <Testimonial></Testimonial>
     </div>
   );
 };
