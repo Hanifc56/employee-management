d7f3e0c315404e09e989ad5a52bc6f570df2f979
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-09 03:17:12 +0600

    image uploded to imgbb

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,31 +1,61 @@
 import { Link, useNavigate } from "react-router-dom";
 
 import { useState } from "react";
-import { Toaster, toast } from "sonner";
+import { Toaster } from "sonner";
 import { updateProfile } from "firebase/auth";
 
 import useAuth from "../../Hooks/useAuth";
 ("use client");
 
 import { HiMail } from "react-icons/hi";
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import {
+  Button,
+  Checkbox,
+  FileInput,
+  Label,
+  Select,
+  TextInput,
+} from "flowbite-react";
 import Swal from "sweetalert2";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 const Register = () => {
   const { createUser } = useAuth();
+  const axiosPublic = useAxiosPublic();
 
   const [passError, setPassError] = useState("");
   const navigate = useNavigate();
 
-  const handleRegister = (e) => {
+  const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+  const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
+  const handleRegister = async (e) => {
     e.preventDefault();
-    console.log(e.currentTarget);
+
     const form = new FormData(e.currentTarget);
+
     const name = form.get("name");
-    const photo = form.get("photo");
     const email = form.get("email");
     const password = form.get("password");
-    console.log(name, photo, email, password);
+    const role = form.get("role");
+    const designation = form.get("designation");
+    const account = form.get("account");
+    const salary = form.get("salary");
+
+    console.log(name, email, password, role, designation, account, salary);
+    // get files from input and send them to imgbb
+    const formData = new FormData();
+    const imageInput = e.target.elements["photo"];
+    const imageFile = imageInput.files[0];
+    formData.append("image", imageFile);
+
+    const res = await axiosPublic.post(image_hosting_api, formData, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    console.log(res.data.data.display_url);
+    // send this url to db to store user data
 
     if (password.length < 6) {
       setPassError("Password should be more then 6 character");
@@ -44,10 +74,17 @@ const Register = () => {
     createUser(email, password)
       .then((result) => {
         console.log(result.user);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "User Created Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        }) && navigate("/");
         // get user data
         updateProfile(result.user, {
           displayName: name,
-          photoURL: photo,
+          photoURL: res.data.data.display_url,
         })
           .then((currentUser) => {
             console.log(currentUser, "Profile Updated");
@@ -55,14 +92,6 @@ const Register = () => {
           .catch((error) => {
             console.error(error);
           });
-
-        Swal.fire(
-          {
-            title: " succefully!",
-            text: "User created succefully!",
-            icon: "success",
-          } && navigate("/")
-        );
       })
       .catch((error) => {
         console.error(error);
@@ -80,32 +109,6 @@ const Register = () => {
           onSubmit={handleRegister}
           className="flex max-w-lg flex-col gap-4"
         >
-          <div className="">
-            <div className="mb-2 block">
-              <Label htmlFor="name" value="Your Name" />
-            </div>
-            <TextInput
-              id="name"
-              type="text"
-              placeholder="Enter you name"
-              name="name"
-              required
-              shadow
-            />
-          </div>
-          <div className="">
-            <div className="mb-2 block">
-              <Label htmlFor="photo" value="Photo URL" />
-            </div>
-            <TextInput
-              id="photo"
-              type="text"
-              placeholder="Enter you photo URl"
-              name="photo"
-              required
-              shadow
-            />
-          </div>
           <div className="">
             <div className="mb-2 block">
               <Label htmlFor="email2" value="Your Email" />
@@ -131,6 +134,7 @@ const Register = () => {
               id="password2"
               name="password"
               type="password"
+              placeholder="Enter your password"
               required
               shadow
             />
@@ -138,6 +142,79 @@ const Register = () => {
               <span className="label-text text-red-400">{passError}</span>
             )}
           </div>
+          <div className="">
+            <div className="mb-2 block">
+              <Label htmlFor="name" value="Your Name" />
+            </div>
+            <TextInput
+              id="name"
+              type="text"
+              placeholder="Enter you name"
+              name="name"
+              required
+              shadow
+            />
+          </div>
+          <div className="">
+            <div className="mb-2 block">
+              <Label htmlFor="designation" value="Select your designation" />
+            </div>
+            <Select id="designation" name="designation" required>
+              <option>Office Manager</option>
+              <option>Administrative Assistant</option>
+              <option>Executive Assistant</option>
+              <option>Office Administrator</option>
+              <option>Office Coordinator</option>
+            </Select>
+          </div>
+          <div className="">
+            <div className="mb-2 block">
+              <Label htmlFor="role" value="Select your Role" />
+            </div>
+            <Select id="role" name="role" required>
+              <option>Employee</option>
+              <option>Hr</option>
+              <option>Admin</option>
+            </Select>
+          </div>
+          <div className="">
+            <div className="mb-2 block">
+              <Label htmlFor="account" value="Your Account" />
+            </div>
+            <TextInput
+              id="account"
+              type="number"
+              placeholder="Example 123456789"
+              name="account"
+              required
+              shadow
+            />
+          </div>
+          <div className="">
+            <div className="mb-2 block">
+              <Label htmlFor="salary" value="Select your Salary" />
+            </div>
+            <Select id="salary" name="salary" required>
+              <option>30,000</option>
+              <option>40,000</option>
+              <option>45,000</option>
+              <option>50,000</option>
+              <option>60,000</option>
+            </Select>
+          </div>
+
+          <div id="fileUpload" className="">
+            <div className="mb-2 block">
+              <Label htmlFor="file" value="Upload photo" />
+            </div>
+            <FileInput
+              id="file"
+              name="photo"
+              helperText="A profile picture is useful to confirm your are logged into your account"
+              required
+            />
+          </div>
+
           <div className="flex items-center gap-2">
             <Checkbox id="agree" />
             <Label htmlFor="agree" className="flex">
